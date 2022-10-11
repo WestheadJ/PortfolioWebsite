0bc0aff20c01ff98da40ca6f35b4af4e61015a4d
James Westhead <57768613+jimbob0119@users.noreply.github.com>
2022-10-11 15:55:23 +0100

    + Files and - Files

---
+++ src/AllProjects.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+
+
+export default function AllProjects() {
+  return (
+    <div className='App' style={{backgroundColor:'#F2E9E4',height:'900px',}}>
+        <h1>My Projects</h1>
+        <hr/>
+        
+    </div>
+  )
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import AllProjects from './AllProjects';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar style={{backgroundColor:'#9A8C98'}} expand="md">
+          <Container>
+            <Navbar.Brand style={{color:'#F2E9E4'}} href="/">James Westhead</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+            <Nav style={{paddingTop:'15px'}} className="me-auto">
+              <Nav.Link href="/"><p style={{color:'#F2E9E4'}}>Home</p></Nav.Link>
+              <NavDropdown title={<span style={{color:'#F2E9E4'}}>Projects</span>}id="basic-nav-dropdown">
+                <NavDropdown.Item ><Link to='/projects/allprojects'>All Projects</Link></NavDropdown.Item>
+                <NavDropdown.Divider/>
+                <NavDropdown.Item href="#action/3.1">A Level Project</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <Routes>
+          <Route exact path='/' element={<Dashboard/>} />
+          <Route exact path='/projects/allprojects' element={<AllProjects/>} />
+          <Route path='/projects/alevelproject'/>
+        </Routes>
+      </Router>  
     </div>
   );
+}
+
+function Dashboard(){
+  return(<div style={{backgroundColor:"#F2E9E4",height:'900px'}}>
+  <section style={{marginTop:"10%", marginLeft:'5%', textAlign:'left'}}>
+    <h1>Hello welcome to my portfolio</h1>
+    <h1>website</h1>
+  </section>
+  <section style={{marginTop:"5%",marginLeft:'5%' , textAlign:'left'}}>
+    <h4 >My name is <strong style={{color:'#4A4E69'}}>James Westhead</strong> </h4>
+  </section>
+</div>)
 }
 
 export default App;
