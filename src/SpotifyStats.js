8bec2a4838a9cab335b1a0ca8280a54712cdc9d1
James Westhead <james_westhead@outlook.com>
2023-10-22 22:44:39 +0100

    Added CSS + changes to spotifystats page

--- src/SpotifyStats.js
+++ src/SpotifyStats.js
@@ -13,23 +13,23 @@ export default function SpotifyStats() {
         <div className='spotifyStatsPage'>
             <h1 style={{ textAlign: 'center', marginTop: '3%', marginBottom: '3%' }}>Spotify Latest</h1>
             <a className="github-link" href="https://github.com/WestheadJ/spotifyLatestRelease/tree/main"><img src={githubIcon} style={{ width: '24px', height: "24px" }} alt="github icon" />View On GitHub</a>
-            <section id='spotifySectionOne'>
-                <h2>Summary</h2>
+            <section id='spotifySummary'>
+                <h4>Summary</h4>
                 <p>The aim was to create a single page application that gets your recent most played artists and songs and also shows new songs that have been released like the release radar feature on spotify. </p>
 
                     {/* <p>I built this single paged application with <a style={{ color: '#5e585d' }} href='https://reactjs.org'>React.js</a> and using the <a style={{ color: '#5e585d' }} href='https://developer.spotify.com/dashboard/'>Spotify API</a>. Spotify keeps user stats on their most recent artists and songs. So I have used the <a style={{ color: '#5e585d' }} href='https://developer.spotify.com/dashboard/'>Spotify API</a> to fetch this information and display to the user.</p> */}
             </section>
-            <section id="spotifySectionTwo">
-                <h2>What Was The Project?</h2>
+            <section id="spotifyDescription">
+                <h4>What Was The Project?</h4>
                 <p>I wanted to learn how to make a single page application with <a style={{ color: '#5e585d' }} href='https://reactjs.org'>React.js</a> and working with a third-party API and displaying the data from it. One of my favorite past times is listening to music, using Spotify as my main platform to listen to music.
                 I was then reminded that there was a web app that was created that I love using from time to time and that is <a style={{color:'#5e585d'}} href='https://www.statsforspotify.com'>Stats for Spotify</a>. This displays data from your spotify account using Spotify's own free to use 
                 <a style={{ color: '#5e585d' }} href='https://developer.spotify.com/dashboard/'>Spotify API</a>. I got to learning how to use the Spotify API first with <a style={{color:'#5e585d'}} href='https://www.postman.com'>PostmanAPI</a> then it was thinking about how I could display it.
                 Now I am not a man who is good at designing, so the design of the page is off however the functionality all works and think the data is shown off well.</p>
             </section>
-            <section id="spotifySectionThree">
-                <h2>What Did I Learn From This?</h2>
-                <p>I learnt a few things and also refreshed my knowledge of some things after this project. One thing I enjoyed learning was how Spotify's own API worked, I liked learning how they structured their return data and what actual data they had. The way they stored it was through dictionaries, so it was a hassle trying to search through the dictionaries all the time.</p>
-                
+            <section id="spotifyLearning">
+                <h4>What Did I Learn From This?</h4>
+                <p>I learnt a few things and also refreshed my knowledge of some things after this project. One thing I enjoyed learning was how Spotify's own API worked, I liked learning how they structured their return data and what actual data they had. The way they stored it was through dictionaries, so it was a hassle trying to search through the dictionaries all the time. I also learnt how react can work with components, with my navbar. </p>
+
             </section>
             {/* <div id='spotifySectionTwo'>
                 <div>
